5ec6c576e1f541c03aefc4a7baa1561c2c46357a
GeloDevDesign <abserenuela@ccc.edu.ph>
2025-07-08 22:20:37 +0800

    asdfasd

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -5,14 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Database\Factories\CategoryFactory; 
 
 class CategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        Category::factory(10)->create();
+        Category::factory()->count(10)->create();
     }
 }
