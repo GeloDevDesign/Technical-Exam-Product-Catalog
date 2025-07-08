919679fcf1c2b9e63c408359bb727acab81aff77
GeloDevDesign <abserenuela@ccc.edu.ph>
2025-07-09 01:18:27 +0800

    ang pogi ko tapos na api ko

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
                     ->symbols()
                     ->letters()
                     ->mixedCase()
-                    ->uncompromised()
+                    // ->uncompromised()
             ],
         ]);
 
--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,12 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::get('/products', [ProductController::class, 'index']);
-
+    Route::post('/products', [ProductController::class, 'store']);
+    
     Route::get('/categories', [CategoryController::class, 'index']);
 
 
     Route::middleware(['can:modify'])->group(function () {
-        Route::apiResource('products', ProductController::class)->except('index');
+        Route::apiResource('products', ProductController::class)->except(['index','store']);
     });
 });
